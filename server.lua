36e6f8d80d4d42730a3194b50f0f3dd043a4aeee
Testarossa <austin.w.burrows@gmail.com>
2022-10-18 04:49:49 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,32 +2,26 @@ GlobalState.GettingRobbed = false
 
 RegisterNetEvent('FinishRobbery')
 AddEventHandler('FinishtRobbery', function()
-	local count = exports.ox_inventory:Search(source, 'count', 'advanced_lockpick')
 	local cashMoney = math.random(200, 500)
 	local chance = math.random(1, 10)
-	if count >= 1 then
-		exports.ox_inventory:RemoveItem(source, 'advanced_lockpick', 1)
-		exports.ox_inventory:AddItem(source, 'money', cashMoney)
-		if chance >= 4 then
-			exports.ox_inventory:AddItem(source, 'safecracker', 1)
-		end
-		TimerThread()
+	exports.ox_inventory:RemoveItem(source, 'advanced_lockpick', 1)
+	exports.ox_inventory:AddItem(source, 'money', cashMoney)
+	if chance >= 4 then
+		exports.ox_inventory:AddItem(source, 'safecracker', 1)
 	end
+	TimerThread()
 end)
 
 RegisterNetEvent('FinishSafeRobbery')
 AddEventHandler('FinishSafeRobbery', function()
-	local count = exports.ox_inventory:Search(source, 'count', 'advanced_lockpick')
 	local cashMoney = math.random(400, 1000)
 	local chance = math.random(1, 10)
-	if count >= 1 then
-		exports.ox_inventory:RemoveItem(source, 'safecracker', 1)
-		exports.ox_inventory:AddItem(source, 'money', cashMoney)
-		if chance >= 4 then
-			exports.ox_inventory:AddItem(source, 'usb_hack', 1)
-		end
-		TimerThread()
+	exports.ox_inventory:RemoveItem(source, 'safecracker', 1)
+	exports.ox_inventory:AddItem(source, 'money', cashMoney)
+	if chance >= 4 then
+		exports.ox_inventory:AddItem(source, 'usb_hack', 1)
 	end
+	TimerThread()
 end)
 
 
