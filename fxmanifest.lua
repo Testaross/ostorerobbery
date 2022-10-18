87c2550c023639c7fb1987db7aed081a923beb8f
Testarossa <austin.w.burrows@gmail.com>
2022-10-18 04:30:17 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,10 @@
 fx_version 'bodacious'
 game 'gta5'
 lua54 'yes'
-author 'Bruno and Bobs Store Robbery bitches'
+author 'Bruno and testarossas Store Robbery bitches'
 version '1.0.0'
 
 
-client_script {
-    'client.lua',
-}
+client_script 'client.lua'
 server_script 'server.lua'
-
-shared_scripts {
-    '@ox_lib/init.lua',
-    'shared.lua'
-}
+shared_script '@ox_lib/init.lua'
