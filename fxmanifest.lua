300a61dcc0fe40f18cc9884158789b7853ed1f18
Testarossa <austin.w.burrows@gmail.com>
2024-01-20 22:20:47 -0500

    2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,17 @@
 fx_version 'bodacious'
 game 'gta5'
 lua54 'yes'
-author 'Bruno and testarossas Store Robbery bitches'
-version '1.0.0'
+author 'testarossas Store Robbery bitches'
+version '2'
 
 
-client_script 'client.lua'
+client_scripts {
+    "client.lua",
+    "targets.lua"
+}
 server_script 'server.lua'
-shared_script '@ox_lib/init.lua'
+
+shared_scripts {
+    "config.lua",
+    "@ox_lib/init.lua"
+}
