c433ab7e482f3a17bc237506399397d8aba612f7
Testarossa <austin.w.burrows@gmail.com>
2024-01-20 22:31:54 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,126 +1,126 @@
 --StoreRobbery
 
-function PerformSkillCheck()
-local skillcheck = false
-if Config.registerSkill == 'ox_lib' then
-    skillcheck = lib.skillCheck({'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy'})
-elseif Config.registerSkill == 'psui' then
-    exports['ps-ui']:Scrambler(function(success)
-	skillcheck = success
-    end, "numeric", 30, 0)
-elseif Config.registerSkill == 'boii' then
-    exports['boii_minigames']:wire_cut({
-	style = 'default',
-	timer = 60000
-    }, function(success)
-	skillcheck = success
-    end)
-end
-return skillcheck
-end
-
-RegisterNetEvent('StoreRobbery')
-AddEventHandler('StoreRobbery', function()
-local policeCount = GlobalState.police or 0
-local state = GlobalState.GettingRobbed
-if state == false then
-    if policeCount >= Config.policeCount then
-	local skillcheck = PerformSkillCheck()
-	if skillcheck then
-	    if lib.progressBar({
-		duration = 20000, 
-		label = 'Grabbing Cash', 
-		useWhileDead = false, 
-		canCancel = true,
-		disable = {car = true,}, 
-		anim = {dict = 'anim@heists@ornate_bank@grab_cash', clip = 'grab'},  flag = 3})
-	    then 
-		TriggerServerEvent("FinishRobbery")
-	    
-	    else 
-		TriggerServerEvent("FailRobbery")
-	    end 
-	else
-	    TriggerServerEvent("FailRobbery")
-	end
-    else
-	lib.notify({
-	    id = 'some_identifier',
-	    title = 'Notifcation',
-	    description = 'Not a good time to be stealing',
-	    position = 'top',
-	    duration = '5000',
-	    style = {
-		backgroundColor = '#141517',
-		color = '#909296'
-	    },
-	    icon = 'ban',
-	    iconColor = '#C53030'
-	})
+    function PerformSkillCheck()
+        local skillcheck = false
+        if Config.registerSkill == 'ox_lib' then
+            skillcheck = lib.skillCheck({'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy'})
+        elseif Config.registerSkill == 'psui' then
+            exports['ps-ui']:Scrambler(function(success)
+                skillcheck = success
+            end, "numeric", 30, 0)
+        elseif Config.registerSkill == 'boii' then
+            exports['boii_minigames']:wire_cut({
+                style = 'default',
+                timer = 60000
+            }, function(success)
+                skillcheck = success
+            end)
+        end
+        return skillcheck
     end
-else
-    print('slow down')
-end
-end)
 
-function PerformSkillCheckSafe()
-local skillchecksafe = false
-
-if Config.registerSkill == 'ox_lib' then
-    skillchecksafe = lib.skillCheck({'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy'})
-elseif Config.registerSkill == 'psui' then
-    exports['ps-ui']:Maze(function(success)
-	if success then
-	    skillchecksafe = success
-	end
-    end, 20) -- Hack Time Limit
-elseif Config.registerSkill == 'boii' then
-    exports['boii_minigames']:wire_cut({
-	style = 'default',
-	timer = 60000
-    }, function(success)
-	skillchecksafe = success
+    RegisterNetEvent('StoreRobbery')
+    AddEventHandler('StoreRobbery', function()
+        local policeCount = GlobalState.police or 0
+        local state = GlobalState.GettingRobbed
+        if state == false then
+            if policeCount >= Config.policeCount then
+                local skillcheck = PerformSkillCheck()
+                if skillcheck then
+                    if lib.progressBar({
+                        duration = 20000, 
+                        label = 'Grabbing Cash', 
+                        useWhileDead = false, 
+                        canCancel = true,
+                        disable = {car = true,}, 
+                        anim = {dict = 'anim@heists@ornate_bank@grab_cash', clip = 'grab'},  flag = 3})
+                    then 
+                        TriggerServerEvent("FinishRobbery")
+                    
+                    else 
+                        TriggerServerEvent("FailRobbery")
+                    end 
+                else
+                    TriggerServerEvent("FailRobbery")
+                end
+            else
+                lib.notify({
+                    id = 'some_identifier',
+                    title = 'Notifcation',
+                    description = 'Not a good time to be stealing',
+                    position = 'top',
+                    duration = 5000,
+                    style = {
+                        backgroundColor = '#141517',
+                        color = '#909296'
+                    },
+                    icon = 'ban',
+                    iconColor = '#C53030'
+                })
+            end
+        else
+            print('slow down')
+        end
     end)
-end
 
-return skillchecksafe
-end
+    function PerformSkillCheckSafe()
+        local skillchecksafe = false
 
-RegisterNetEvent('SafeRobbery')
-AddEventHandler('SafeRobbery', function()
-local policeCount = GlobalState.police or 0
-if policeCount >= Config.policeCount then
-    local result = PerformSkillCheckSafe()
-    if result then
-	if lib.progressBar({
-	    duration = 20000, 
-	    label = 'Robbing Safe', 
-	    useWhileDead = false, 
-	    canCancel = false,
-	    disable = {car = true,}, 
-	    anim = {dict = 'anim@heists@ornate_bank@grab_cash', clip = 'grab'},  flag = 3})
-	then 
-	    TriggerServerEvent("FinishSafeRobbery")
-	end 
-    else
-	TriggerServerEvent("FailRobbery")
+        if Config.registerSkill == 'ox_lib' then
+            skillchecksafe = lib.skillCheck({'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy', 'easy'})
+        elseif Config.registerSkill == 'psui' then
+            exports['ps-ui']:Maze(function(success)
+                if success then
+                    skillchecksafe = success
+                end
+            end, 20) -- Hack Time Limit
+        elseif Config.registerSkill == 'boii' then
+            exports['boii_minigames']:wire_cut({
+                style = 'default',
+                timer = 60000
+            }, function(success)
+                skillchecksafe = success
+            end)
+        end
+
+        return skillchecksafe
     end
-else
-    lib.notify({
-	id = 'lolwhat',
-	title = 'Notifcation',
-	description = 'Not a good time to be stealing',
-	position = 'top',
-	duration = '5000',
-	style = {
-	    backgroundColor = '#141517',
-	    color = '#909296'
-	},
-	icon = 'ban',
-	iconColor = '#C53030'
-    })
-end
-end)
+
+    RegisterNetEvent('SafeRobbery')
+    AddEventHandler('SafeRobbery', function()
+        local policeCount = GlobalState.police or 0
+        if policeCount >= Config.policeCount then
+            local result = PerformSkillCheckSafe()
+            if result then
+                if lib.progressBar({
+                    duration = 20000, 
+                    label = 'Robbing Safe', 
+                    useWhileDead = false, 
+                    canCancel = false,
+                    disable = {car = true,}, 
+                    anim = {dict = 'anim@heists@ornate_bank@grab_cash', clip = 'grab'},  flag = 3})
+                then 
+                    TriggerServerEvent("FinishSafeRobbery")
+                end 
+            else
+                TriggerServerEvent("FailRobbery")
+            end
+        else
+            lib.notify({
+                id = 'lolwhat',
+                title = 'Notifcation',
+                description = 'Not a good time to be stealing',
+                position = 'top',
+                duration = 5000,
+                style = {
+                    backgroundColor = '#141517',
+                    color = '#909296'
+                },
+                icon = 'ban',
+                iconColor = '#C53030'
+            })
+        end
+    end)
 
     
 
